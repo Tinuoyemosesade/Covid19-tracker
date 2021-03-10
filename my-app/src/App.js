d92ef0e7330389f0e25afb9a1562e168f014aef6
Moses Tinuoye <tinuoyemosesade@gmail.com>
2021-03-10 17:38:13 +0100

    wip

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from './app.module.css';
+
 class app extends React.Component{
   render() {
     return (
-      <div>
-        <h1>App</h1>
+      <div className={styles.container}>
+        <Cards />
+        <CountryPicker />
+        <Chart />
       </div>
     )
   }
