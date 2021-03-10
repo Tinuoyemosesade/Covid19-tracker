4c03c1e4ed5b6d68f1901072e4f5f5292ab25d7c
Moses Tinuoye <tinuoyemosesade@gmail.com>
2021-03-10 18:00:55 +0100

    api.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-import { Cards, Chart, CountryPicker } from "./components";
+import { Cards, Chart, CountryPicker } from './components';
 import styles from './app.module.css';
+import { fetchData } from './api';
 
 class app extends React.Component{
+
+async componentDidMount() {
+  const data = await fetchData();
+
+  console.log(data);
+}
   render() {
     return (
       <div className={styles.container}>
